95c65c161c64693666ca2d94846079262496164e
Alvaro F <alvaro.fromero@gmail.com>
2023-04-05 09:23:12 +0200

    Add project files.

---
+++ CustomerOrderViewer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomerOrderViewer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
